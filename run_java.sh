3d554dd74f2a64c2315e3925b590ec4f2d8dda8a
Chris Smith <chrsmith@google.com>
2014-03-14 14:45:19 -0700

    Update run_java.sh

--- run_java.sh
+++ run_java.sh
@@ -1,4 +1,4 @@
 # Manually update:
-# ../sdks/java_sdk/appengine-java-sdk-1.9.0/bin/appcfg.sh --oauth2 update ./target/bloopy-fish-1
-../sdks/java_sdk/appengine-java-sdk-1.9.0/bin/dev_appserver.sh ./target/bloopy-fish-1
+# ../sdks/java_sdk/appengine-java-sdk-1.9.0/bin/appcfg.sh --oauth2 update ./target/flapping-bird-1
+../sdks/java_sdk/appengine-java-sdk-1.9.0/bin/dev_appserver.sh ./target/flapping-bird-1
 
