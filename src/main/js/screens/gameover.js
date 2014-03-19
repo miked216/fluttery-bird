39fbc0abbf641a61ef0c824a236af15607902714
Chris Smith <chrsmith+github@google.com>
2014-03-18 17:11:27 -0700

    Touch CSS, change wording

--- src/main/js/screens/gameover.js
+++ src/main/js/screens/gameover.js
@@ -68,8 +68,8 @@ game.GameOverScreen = me.ScreenObject.extend({
           // renderable
           this.parent(new me.Vector2d(), 100, 100);
           this.font = new me.Font('gamefont', 40, 'black', 'left');
-          this.steps = 'Steps: ' + game.data.steps.toString();
-          this.topSteps= 'Higher Step: ' + me.save.topSteps.toString();
+          this.steps = 'Score: ' + game.data.steps.toString();
+          this.topSteps= 'Your best: ' + me.save.topSteps.toString();
       },
 
       update: function () {
