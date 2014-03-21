4bc49c1739d4341bbd123db281b06494778ce1ec
Chris Smith <chrsmithdemos@gmail.com>
2014-03-21 20:22:08 +0000

    Fix the demo, save the world!

--- src/main/java/com/google/games/flutterybird/HighScoreService.java
+++ src/main/java/com/google/games/flutterybird/HighScoreService.java
@@ -80,13 +80,6 @@ public class HighScoreService {
   }
 
   public void addNewScore(DatastoreService datastoreService, HighScore newScore) {
-    // BUG FIX: This app is woefully insecure, anybody can just
-    // HTTP POST "/highscore?score=999999"!
-    // Fix this by only allowing application administrators to post high scores.
-    UserService userService = UserServiceFactory.getUserService();
-    if (userService.getCurrentUser() == null || !userService.isUserAdmin()) {
-      throw new RuntimeException("Non-admin user attempting to upload high score!");
-    }
 
     // TODO(chris): User a proper escaping library.
     String name = newScore.getPlayer()
